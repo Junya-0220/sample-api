a8a839d6773285111542c1fecd5c285121614cbc
Junya-0220 <junya010220@gmail.com>
2022-10-26 16:06:03 +0900

    ログ追加

--- main.go
+++ main.go
@@ -9,9 +9,11 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/hello", func(ctx *gin.Context) {
+	router.GET("/hello/2", func(ctx *gin.Context) {
 		ctx.String(200, "Hello World")
 	})
+
+	log.Print("my first log")
 
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal("Server Run Failed.: ", err)
